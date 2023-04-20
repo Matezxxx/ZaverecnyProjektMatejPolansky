d7e16d071f50eacac68942844e5df83d1c20bd13
Matezxxx <Polansky.mat@seznam.cz>
2023-04-20 15:09:43 +0200

    Zkouska

---
+++ ZaverecnyProjektPolanskyMatej/Metody.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaverecnyProjektPolanskyMatej
+{
+    internal class Metody
+    {
+        public List<Pojistenec> seznamPojistenych = new List<Pojistenec>();
+        public void VytvorPojisteneho(string jmeno, string prijmeni, int vek, string telefon)
+        {
+            Pojistenec pojisteny = new Pojistenec(jmeno, prijmeni, vek, telefon);
+
+            seznamPojistenych.Add(pojisteny);
+            Console.WriteLine("\n Data byla uložena, uložili jste nového pojištěného {0} {1}, pokračujte libovolnou klávesou...\n", jmeno, prijmeni);
+        }
+        public void VypisSeznamPojistenych()
+        {
+            Console.WriteLine("\nSeznam pojištěných osob:");
+            foreach (Pojistenec pojisteny in seznamPojistenych)
+            {
+                Console.WriteLine(pojisteny.ToString());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pokračujte libovolnou klávesou");
+        }
+        public void NajdiPojisteneho(string jmeno, string prijmeni)
+        {
+            foreach (Pojistenec pojisteny in seznamPojistenych)
+            {
+                if (pojisteny.Jmeno == jmeno && pojisteny.Prijmeni == prijmeni)
+                {
+
+                    Console.WriteLine("\nPojištěný {0} {1} byl nalezen:\n{2}\n", jmeno, prijmeni, pojisteny.ToString());
+                    Console.WriteLine("Pokračujte libovolnou klávesou");
+                }
+                else
+                {
+                    Console.WriteLine("\nPojištěný {0} {1} nebyl nalezen.\n", jmeno, prijmeni);
+                    Console.WriteLine("Pokračujte libovolnou klávesou");
+                }
+            }
+        }
+    }
+}
---
+++ ZaverecnyProjektPolanskyMatej/Program.cs
@@ -0,0 +1,58 @@
+﻿
+using ZaverecnyProjektPolanskyMatej;
+
+Metody metody = new Metody();
+while (true)
+{
+    Console.WriteLine("-------------------------------");
+    Console.WriteLine("Evidence pojištěných");
+    Console.WriteLine("-------------------------------");
+    Console.WriteLine();
+    Console.WriteLine("Vyberte možnost:");
+    Console.WriteLine("1 - Přidat nového pojištěného");
+    Console.WriteLine("2 - Vypsat všechny pojištěné");
+    Console.WriteLine("3 - Vyhledání pojištěného");
+    Console.WriteLine("4 - Konec");
+
+    int volba;
+    while (!int.TryParse(Console.ReadLine(), out volba))
+    {
+        Console.WriteLine("Neplatná volba. Zadejte číslo od 1 do 4.");
+    }
+    switch (volba)
+    {
+        case 1:
+            Console.Write("Zadejte jméno pojištěného: ");
+            string jmeno = Console.ReadLine();
+
+            Console.Write("Zadejte příjmení pojištěného: ");
+            string prijmeni = Console.ReadLine();
+
+            Console.Write("Zadejte věk pojištěného: ");
+            int vek = int.Parse(Console.ReadLine());
+
+            Console.Write("Zadejte telefonní číslo pojištěného: ");
+            string telefon = Console.ReadLine();
+
+            metody.VytvorPojisteneho(jmeno, prijmeni, vek, telefon);
+            Console.ReadKey();
+            break;
+        case 2:
+            metody.VypisSeznamPojistenych();
+            Console.ReadKey();
+            break;
+        case 3:
+            Console.Write("Zadejte jméno a příjmení pojištěného oddělené mezerou: ");
+            string[] jmenoPrijmeni = Console.ReadLine().Split(" ");
+
+            metody.NajdiPojisteneho(jmenoPrijmeni[0], jmenoPrijmeni[1]);
+            Console.ReadKey();
+            break;
+        case 4:
+            Console.WriteLine("Konec programu.");
+            return;
+        default:
+            Console.WriteLine("Neplatná volba.");
+            break;
+    }
+}
